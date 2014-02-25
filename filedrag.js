391828c2dc15f3be94037ffed0fe3f593ab4e7fa
tjoker <clement-lavabre@hotmail.fr>
2014-02-25 11:27:04 +0100

    Upload des fichiers :
    - filedrag.js
    - index.html
    - style.css
    
    index.html : permet de prendre des fichiers et de les transformer en datauri.
    style.css  : correspond à le mise en forme de la page html
    filedrag.js : permet la transforamtion d'un fichier ( html,css,jpg,js...) en data URI.

---
+++ filedrag.js
@@ -0,0 +1,96 @@
+
+(function() {
+
+	// getElementById
+	function $id(id) {
+		return document.getElementById(id);
+	}
+
+
+	// output information
+	function Output(msg) {
+		var m = $id("messages");
+		m.innerHTML = msg;
+	}
+
+
+	// file drag hover
+	function FileDragHover(e) {
+		e.stopPropagation();
+		e.preventDefault();
+		e.target.className = (e.type == "dragover" ? "hover" : "");
+	}
+
+
+	// file selection
+	function FileSelectHandler(e) {
+
+		// cancel event and hover styling
+		FileDragHover(e);
+
+		// fetch FileList object
+		var files = e.target.files || e.dataTransfer.files;
+
+		// process all File objects
+		for (var i = 0, f; f = files[i]; i++) {
+			ParseFile(f);
+		}
+
+	}
+
+	function ParseFile(file)
+	{
+	var ext = file.name.split(".").pop().toLowerCase();
+	var selectedFile = file;
+	var reader = new FileReader();
+
+	reader.onload = function() {
+		if(ext!='smil')
+			dataURL = reader.result;
+		else
+			dataURL = 'data:application/smil+xml;'+reader.result.split(";").pop();
+		console.log(dataURL);
+		Output(				"<p><strong>" + file.name + ":</strong></p><pre>" +
+					dataURL+
+					"</pre>"
+				);
+	};
+	reader.readAsDataURL(selectedFile);
+	//return false;
+		
+	}
+
+
+	// initialize
+	function Init() {
+
+		var fileselect = $id("fileselect"),
+			filedrag = $id("filedrag"),
+			submitbutton = $id("submitbutton");
+
+		// file select
+		fileselect.addEventListener("change", FileSelectHandler, false);
+
+		// is XHR2 available?
+		var xhr = new XMLHttpRequest();
+		if (xhr.upload) {
+
+			// file drop
+			filedrag.addEventListener("dragover", FileDragHover, false);
+			filedrag.addEventListener("dragleave", FileDragHover, false);
+			filedrag.addEventListener("drop", FileSelectHandler, false);
+			filedrag.style.display = "block";
+
+			// remove submit button
+			submitbutton.style.display = "none";
+		}
+
+	}
+
+	// call initialization file
+	if (window.File && window.FileList && window.FileReader) {
+		Init();
+	}
+
+
+})();
